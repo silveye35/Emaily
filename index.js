e98e842b4881355b13e52e60ee93f7c8cefbfc83
Edgar Silveyra <phoenixdeath35@phoenixsmacbook.home>
2020-03-30 00:40:04 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// To run, do the following:
+// type in terminal 'node index.js'
+// open up browser and type in 'localhost:5000'
+
+const express = require('express'); //imports express servers
+const app = express();
+
+app.get('/', (rec, res) => {
+    res.send({ hi: 'there' })
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT); // tells node to listen to traffic coming through port 5000
+
