f3a4fd1e3165b38112b20bf37695cd20480b312a
Edgar Silveyra <silveyra.edgar14@gmail.com>
2020-08-03 21:11:28 -0700

    Added authentication and enabled cookies for users

--- index.js
+++ index.js
@@ -7,12 +7,24 @@
 
 const express = require('express'); //imports express servers
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session'); 
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days represented in milliseconds
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
