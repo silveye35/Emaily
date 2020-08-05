ecf22a246336641f665c76c6cb7b3d2a6580acb1
Edgar Silveyra <silveyra.edgar14@gmail.com>
2020-08-05 10:47:11 -0700

    added route handler to log users out

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,6 +9,11 @@ module.exports = app => {
     
     app.get('/auth/google/callback', passport.authenticate('google'));
 
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send(req.user); // gets message saying 'undefined' to notify user they are logged out. Or no content
+    })
+
     app.get('/api/current_user', (req, res) => {
         res.send(req.user);
     });
