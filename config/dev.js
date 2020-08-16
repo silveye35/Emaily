6ec63abb0248ac19d30b9ff9b0af4ac92c105eb1
Edgar Silveyra <silveyra.edgar14@gmail.com>
2020-08-16 14:38:19 -0700

    added dev and prod environments

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+// dev.js - don't commit this!!!
+
+module.exports = {
+    googleClientID: '978654896285-hn3k0enjqjjbgl3isa0nuac1l5hct5s0.apps.googleusercontent.com', 
+    googleClientSecret: 'E0vU18e_CxMxyWJHE1dS_hpa',
+    mongoURI: 'mongodb+srv://poogs1:QQWAf2uGlLMeRfoU@emaily.c9wyd.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'redacted',
+
+};
