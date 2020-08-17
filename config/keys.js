b40929a475eb7f667b5faaf69c9ac0ac7a80d2a8
Edgar Silveyra <silveyra.edgar14@gmail.com>
2020-08-16 20:35:05 -0700

    cleaned up keys.js

--- config/keys.js
+++ config/keys.js
@@ -15,11 +15,3 @@ if (process.env.NODE_ENV === 'production') {
     module.exports = require('./dev');
 
 }
-
-module.exports = {
-    googleClientID: '978654896285-hn3k0enjqjjbgl3isa0nuac1l5hct5s0.apps.googleusercontent.com', 
-    googleClientSecret: 'E0vU18e_CxMxyWJHE1dS_hpa',
-    mongoURI: 'mongodb+srv://poogs1:QQWAf2uGlLMeRfoU@emaily.c9wyd.mongodb.net/test?retryWrites=true&w=majority',
-    cookieKey: 'redacted',
-
-};
